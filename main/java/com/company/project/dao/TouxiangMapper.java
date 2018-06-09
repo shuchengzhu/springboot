30444fcd91b1ffde49a7d8328f803be1b8edb1b4
shuchengzhu <38305376+shuchengzhu@users.noreply.github.com>
2018-06-09 09:32:30 +0800

    Create TouxiangMapper.java

---
+++ main/java/com/company/project/dao/TouxiangMapper.java
@@ -0,0 +1,7 @@
+package com.company.project.dao;
+
+import com.company.project.core.Mapper;
+import com.company.project.model.Touxiang;
+
+public interface TouxiangMapper extends Mapper<Touxiang> {
+}
